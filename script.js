cc71182e0b9e5c3ad23cea395432d14d909f6e20
llldayanlll <142857893+llldayanlll@users.noreply.github.com>
2025-06-05 18:49:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Insert all images automatically from the photos folder
 const gallery = document.querySelector('.gallery');
-const totalImages = 10; // Change based on number of photos
+const totalImages = 6; // Change based on number of photos
 
 for (let i = 1; i <= totalImages; i++) {
   const img = document.createElement('img');
